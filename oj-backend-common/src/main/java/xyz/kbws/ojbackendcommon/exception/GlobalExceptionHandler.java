fc7b1d36f2f5f63877579ca0c4aed1f72e50fcda
kbws <hsy040506@163.com>
2023-11-13 21:13:55 +0800

    common模块和model模块完成

---
+++ oj-backend-common/src/main/java/xyz/kbws/ojbackendcommon/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package xyz.kbws.ojbackendcommon.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import xyz.kbws.ojbackendcommon.common.BaseResponse;
+import xyz.kbws.ojbackendcommon.common.ErrorCode;
+import xyz.kbws.ojbackendcommon.common.ResultUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
