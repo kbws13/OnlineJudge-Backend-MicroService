8a4f35569217918c5b353e0dba6e07fd2d71f0e4
空白无上 <hsy040506@163.com>
2024-02-17 08:35:03 +0800

    初始化时，创建死信队列

--- oj-backend-judge-service/src/main/java/xyz/kbws/ojbackendjudgeservice/rabbitmq/InitRabbitMq.java
+++ oj-backend-judge-service/src/main/java/xyz/kbws/ojbackendjudgeservice/rabbitmq/InitRabbitMq.java
@@ -5,6 +5,11 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static xyz.kbws.ojbackendcommon.constant.MqConstant.*;
+
 /**
  * @author kbws
  * @date 2023/11/18
@@ -16,16 +21,31 @@ public class InitRabbitMq {
     public static void doInit(){
         try {
             ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
+            // todo 配服务MQ信息
+            factory.setHost("xxxxxxx");
+            factory.setPassword("xxxx");
+            factory.setUsername("xxxx");
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
-            String EXCHANGE_NAME = "code_exchange";
-            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+            String codeExchangeName = CODE_EXCHANGE_NAME;
+            channel.exchangeDeclare(codeExchangeName, CODE_DIRECT_EXCHANGE);
 
-            // 创建队列，随机分配一个队列名称
-            String queueName = "code_queue";
-            channel.queueDeclare(queueName, true, false, false, null);
-            channel.queueBind(queueName, EXCHANGE_NAME, "my_routingKey");
+            // 创建 code 队列
+            String codeQueue = CODE_QUEUE;
+            Map<String, Object> codeMap = new HashMap<>();
+
+            // code队列绑定死信交换机
+            codeMap.put("x-dead-letter-exchange", CODE_DLX_EXCHANGE);
+            codeMap.put("x-dead-letter-routing-key", CODE_DLX_ROUTING_KEY);
+            channel.queueDeclare(codeQueue, true, false, false, codeMap);
+            channel.queueBind(codeQueue, codeExchangeName, CODE_ROUTING_KEY);
+
+            // 创建死信队列和死信交换机
+            // 创建死信队列
+            channel.queueDeclare(CODE_DLX_QUEUE, true, false, false, null);
+            // 创建死信交换机
+            channel.exchangeDeclare(CODE_DLX_EXCHANGE, CODE_DIRECT_EXCHANGE);
+            channel.queueBind(CODE_DLX_QUEUE, CODE_DLX_EXCHANGE, CODE_DLX_ROUTING_KEY);
             log.info("消息队列启动成功");
         }catch (Exception e){
             log.error("消息队列启动失败");
