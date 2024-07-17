67e64d2086f88b7ffd171128e6913da240591a61
空白无上 <hsy040506@163.com>
2024-07-17 16:47:51 +0800

    添加SSE，实现判题过程实时通知 format

--- oj-backend-model/src/main/java/xyz/kbws/ojbackendmodel/model/entity/UserCode.java
+++ oj-backend-model/src/main/java/xyz/kbws/ojbackendmodel/model/entity/UserCode.java
@@ -11,36 +11,31 @@ import java.util.Date;
  * @date 2024/2/27
  * @description:
  */
-@TableName(value ="user_code")
+@TableName(value = "user_code")
 @Data
 public class UserCode implements Serializable {
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
     /**
      * id
      */
     @TableId(type = IdType.AUTO)
     private Long id;
-
     /**
      * 用户id
      */
     private Long userId;
-
     /**
      * 创建时间
      */
     private Date createTime;
-
     /**
      * 更新时间
      */
     private Date updateTime;
-
     /**
      * 是否删除
      */
     @TableLogic
     private Integer isDelete;
-
-    @TableField(exist = false)
-    private static final long serialVersionUID = 1L;
 }
